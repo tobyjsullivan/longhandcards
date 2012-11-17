12714569c7b89701a1eaf74240abccb8029cbb2d
Toby Sullivan <tobyjsullivan@gmail.com>
2012-11-17 15:48:32 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Personal Christmas Cards - PersonalChristmasCards.com</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+	<link rel="stylesheet" href="stylesheets/greeting-home.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<!-- Delete everything in this .container and get started on your own site! -->
+
+	<div class="container">
+
+		<div class="sixteen columns">
+			<h1 class="remove-bottom" style="margin-top: 40px">PersonalChristmasCards.com</h1>
+			<h5>It's like having your secretary send a card &mdash; but more personal.</h5>
+			<hr />
+		</div>
+
+		<div class="two-thirds column">
+			<h3>Step One: Choose a Card</h3>
+			<?php
+			for($i = 0; $i < 3 ; $i++) {
+			?>
+			<div class="row card-listing">
+				<div class="four columns alpha">
+					<p class="card-preview">
+						<img src="images/placeholder_card.png">
+					</p>
+				</div>
+				<div class="four columns omega">
+					<div class="card-options">
+						<p>See Inside</p>
+						<p><a href="personalise.php?card=0001">Choose This Card</a></p>
+					</div>
+				</div>
+				<div class="two columns">
+					<p class="card-price">$10</p>
+				</div>
+			</div>
+			<?php
+			}
+			?>
+		</div>
+
+		<div class="one-third column">
+			<h3>How It Works</h3>
+			<p><strong>Step One:</strong> Choose a Card</p>
+			<p><strong>Step Two:</strong> Personalise Your Card</p>
+			<p><strong>Step Three:</strong> Pay</p>
+			<p><strong>Then:</strong> We handwrite your card and mail it in time for Christmas!</p>
+		</div>
+		
+	</div><!-- container -->
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
---
+++ personalise.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Personal Christmas Cards - PersonalChristmasCards.com</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+	<link rel="stylesheet" href="stylesheets/greeting-personalise.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<!-- Delete everything in this .container and get started on your own site! -->
+
+	<div class="container">
+
+		<div class="sixteen columns">
+			<h1 class="remove-bottom" style="margin-top: 40px">PersonalChristmasCards.com</h1>
+			<h5>It's like having your secretary send a card &mdash; but more personal.</h5>
+			<hr />
+		</div>
+
+		<div class="two-thirds column">
+			<h3>Step Two: Personalise Your Card</h3>
+			<p>
+				<img src="images/placeholder_card_message.png" />
+			</p>
+			<form method="post" action="payment.php">
+				<label for="message">Write a message</label>
+				<textarea name="message" id="message">Dear friend,
+May you have a very merry Christmas!
+
+Love,
+John</textarea>
+				<fieldset>
+					<label for="">Choose a writer</label>
+					<label for="writer-toby"><input type="radio" id="writer-toby" name="writer" value="toby" checked="checked" />Toby</label>
+					<label for="writer-hailey"><input type="radio" id="writer-hailey" name="writer" value="hailey" />Hailey</label>
+				</fieldset>
+
+				<fieldset>
+					<label for="">Recipient (Who is the card for?)</label>
+					
+					<label for="recipient-name">Name</label>
+					<input type="text" id="recipient-name" name="recipient-name" />
+					
+					<label for="recipient-address">Address</label>
+					<input type="text" id="recipient-address" name="recipient-address" />
+					
+					<label for="recipient-address2">Address (Line 2)</label>
+					<input type="text" id="recipient-address2" name="recipient-address2" />
+					
+					<label for="recipient-city">City</label>
+					<input type="text" id="recipient-city" name="recipient-city" />
+					
+					<label for="recipient-province">State/Province</label>
+					<input type="text" id="recipient-province" name="recipient-province" />
+
+					<label for="recipient-country">Country</label>
+					<select id="recipient-country" name="recipient-country">
+						<option value="canada">Canada</option>
+						<option value="united-states" selected="selected">United States</option>
+					</select>
+				</fieldset>
+
+				<fieldset>
+					<label for="">Your information</label>
+					<label for="sender-name">Full Name</label>
+					<input type="text" id="sender-name" name="sender-name" />
+					<label for="sender-email">Email</label>
+					<input type="email" id="sender-email" name="sender-email" />
+				</fieldset>
+				<p><input type="submit" value="Proceed to Payment" /></p>
+			</form>
+		</div>
+
+		<div class="one-third column">
+			<h3>How It Works</h3>
+			<p><strong>Step One:</strong> Choose a Card</p>
+			<p><strong>Step Two:</strong> Personalise Your Card</p>
+			<p><strong>Step Three:</strong> Pay</p>
+			<p><strong>Then:</strong> We handwrite your card and mail it in time for Christmas!</p>
+		</div>
+		
+	</div><!-- container -->
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
