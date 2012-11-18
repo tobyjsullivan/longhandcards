390c03bf023eb4e70f77a8b5d20cf86f39dc740d
Toby Sullivan <tobyjsullivan@gmail.com>
2012-11-17 21:38:12 -0800

    Added Stripe live keys and debug config to support

--- stripe_config.php
+++ stripe_config.php
@@ -1,10 +1,18 @@
 <?php
+require_once('debug_config.php');
 require_once('stripe/Stripe.php');
 
-$stripe = array(
-  'secret_key'      => "sk_test_k5bYr7JsouTTQK88EDntN8Pg",
-  'publishable_key' => "pk_test_TsKKWJQMzpvKFjvVrTxbnSy6"
-);
+if($debug >= 1) {
+	$stripe = array(
+	  'secret_key'      => "sk_test_k5bYr7JsouTTQK88EDntN8Pg",
+	  'publishable_key' => "pk_test_TsKKWJQMzpvKFjvVrTxbnSy6"
+	);
+} else {
+	$stripe = array(
+	  'secret_key'      => "sk_live_bqq3vFhzlKb4OL4ktfM5BV9k",
+	  'publishable_key' => "pk_live_RjztYyQW0AdwVKN1oGPB2bXV"
+	);
+}
 
 Stripe::setApiKey($stripe['secret_key']);
 ?>
