5d1bd6de196eb258636205a542eb8fe11973b313
Toby Sullivan <tobyjsullivan@gmail.com>
2012-11-17 23:30:44 -0800

    Updated Adwords conversion script to only be active on production site

---
+++ adword_conversion.php
@@ -0,0 +1,26 @@
+<?php
+require_once('debug_config.php');
+
+if($debug == 0) {
+?>
+<!-- Google Code for Order Completed Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 992228854;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "O3J3CIqigwYQ9uuQ2QM";
+var google_conversion_value = 10;
+/* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/992228854/?value=10&amp;label=O3J3CIqigwYQ9uuQ2QM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+<?php
+}
+?>
