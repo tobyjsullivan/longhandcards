4a5fd532bff0c8588e459b388f85ccf1a636cdf8
Toby Sullivan <tobyjsullivan@gmail.com>
2012-11-17 22:22:57 -0800

    Added Google Analytics tracking code

--- header.php
+++ header.php
@@ -1,3 +1,25 @@
+<?php
+require_once('debug_config.php');
+
+if($debug == 0) {
+?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36415481-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<?php
+}
+?>
+
 <div class="sixteen columns">
 	<h1 class="remove-bottom" style="margin-top: 40px">Personal Christmas Cards</h1>
 	<p><em>Presented by LonghandCards.com</em></p>
