91ccc04cff4da4a4a5f09e74384a97a61b20b15f
Toby Sullivan <tobyjsullivan@gmail.com>
2012-11-17 23:26:26 -0800

    Added Adwords conversion tracking code

--- thankyou.php
+++ thankyou.php
@@ -38,6 +38,25 @@ require_once('Order.php');
 	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
 
+
+	<!-- Google Code for Order Completed Conversion Page -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = 992228854;
+	var google_conversion_language = "en";
+	var google_conversion_format = "3";
+	var google_conversion_color = "ffffff";
+	var google_conversion_label = "O3J3CIqigwYQ9uuQ2QM";
+	var google_conversion_value = 10;
+	/* ]]> */
+	</script>
+	<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+	<div style="display:inline;">
+	<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/992228854/?value=10&amp;label=O3J3CIqigwYQ9uuQ2QM&amp;guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
 </head>
 <body>
 
